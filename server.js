a80c724b92642453c205a19d380592e3ae594f0f
Loreleine Le Clerc <loreleine.leclerc@decathlon.com>
2017-12-22 13:25:30 +0100

    add process env port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 const app = express();
 
-const port = 3000;
+const port = process.env["PORT"] || 3000;
 
 nunjucks.configure("views", {
   autoescape:true,
